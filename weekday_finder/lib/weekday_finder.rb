5070e10f2b7603379465bde1113971227b4392a6
Guest User <student@epicodus.com>
2015-04-21 11:52:05 -0700

    Replaced original test code and allowed user input or test input to be used.

--- weekday_finder/lib/weekday_finder.rb
+++ weekday_finder/lib/weekday_finder.rb
@@ -1,11 +1,26 @@
 class Array
     define_method(:weekday_finder) do
+      year = self[0]
+      month = self[1]
+      day = self[2]
+
+      puts "If you don't enter dates, a default will be used."
       puts "Enter year:"
-      year = gets.to_i()
+      year2 = gets.to_i()
+      if year2>0
+        year = year2
+      end
       puts "Enter month:"
-      month = gets.to_i()
+      month2 = gets.to_i()
+      if month2>0
+        month = month2
+      end
       puts "Enter day:"
-      day = gets.to_i()
+      day2 = gets.to_i()
+      if day2>0
+        day = day2
+      end
+
       newdate = Time.new(year, month, day)
       if newdate.wday == 0
         dayofweek = "Sunday"
