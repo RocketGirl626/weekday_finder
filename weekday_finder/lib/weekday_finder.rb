bb0d09a451f5aa22e561d6bf9d1c1e4c242d1ccc
Guest User <student@epicodus.com>
2015-04-21 10:58:59 -0700

    Successful first date-find.

--- weekday_finder/lib/weekday_finder.rb
+++ weekday_finder/lib/weekday_finder.rb
@@ -1,9 +1,13 @@
-class String
+class Array
     define_method(:weekday_finder) do
-      newdate = Time.new(self)
+      year = self[0]
+      month = self[1]
+      day = self[2]
+      newdate = Time.new(year, month, day)
       if newdate.wday == 2
         dayofweek = "Tuesday"
       end
+    puts (dayofweek)
     dayofweek
   end
 end
--- weekday_finder/spec/weekday_finder_spec.rb
+++ weekday_finder/spec/weekday_finder_spec.rb
@@ -3,8 +3,8 @@ require('weekday_finder')
 
 # find out what day of the week a given date is
 
-describe('String#weekday_finder') do
+describe('Array#weekday_finder') do
   it("finds out what day of the week a given date is") do
-    expect(("2015,4,21").weekday_finder()).to(eq("Tuesday"))
+    expect(([2015,4,21]).weekday_finder()).to(eq("Tuesday"))
   end
 end
