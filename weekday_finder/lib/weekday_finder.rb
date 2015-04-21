11a30b41124cc3b4437086bbcb7d2f088ef9e54e
Guest User <student@epicodus.com>
2015-04-21 11:25:13 -0700

    Program accepts user input and calculates day of the week.

--- weekday_finder/lib/weekday_finder.rb
+++ weekday_finder/lib/weekday_finder.rb
@@ -1,11 +1,26 @@
 class Array
     define_method(:weekday_finder) do
-      year = self[0]
-      month = self[1]
-      day = self[2]
+      puts "Enter year:"
+      year = gets.to_i()
+      puts "Enter month:"
+      month = gets.to_i()
+      puts "Enter day:"
+      day = gets.to_i()
       newdate = Time.new(year, month, day)
-      if newdate.wday == 2
+      if newdate.wday == 0
+        dayofweek = "Sunday"
+      elsif newdate.wday == 1
+        dayofweek = "Monday"
+      elsif newdate.wday == 2
         dayofweek = "Tuesday"
+      elsif newdate.wday == 3
+        dayofweek = "Wednesday"
+      elsif newdate.wday == 4
+        dayofweek = "Thursday"
+      elsif newdate.wday == 5
+        dayofweek = "Friday"
+      elsif newdate.wday == 6
+        dayofweek = "Saturday"
       end
     puts (dayofweek)
     dayofweek
